0a13c56d08ecd85989a34575b9b5a7ac97d39548
AllenYGY <allenygy@outlook.com>
2024-03-24 01:30:34 +0800

    Complete PA1

--- ProgrammingAssignment/PA1/sudoku.c
+++ ProgrammingAssignment/PA1/sudoku.c
@@ -4,101 +4,82 @@
 #include <string.h>
 #include <stdbool.h>
 
+// Define the number of threads
+#define NUM_THREADS 11
+// Define the size of the sudoku
+#define SIZE 9
+
+// Use global variables to store the sudoku matrix and thread ID
+int sudoku[SIZE][SIZE];
+pthread_t tid[NUM_THREADS];
+
+// Define the parameter structure
 typedef struct
 {
     int row;
     int column;
 } parameters;
 
-int isValid = 0;
+// Declare thread functions
+// Check the row
 void *checkRow(void *param);
+// Check the column
 void *checkColumn(void *param);
+// Check the square
 void *checkSquare(void *param);
-
-int sudoku[9][9];
 
 int main(int argc, char *argv[])
 {
-    pthread_t tid[11];
-    pthread_attr_t attr; // 线程属性
-
-    char *token;
+    pthread_attr_t attr; // Set the thread attribute
+    char *token;  // Define the token
     if (argc != 2)
     {
-        fprintf(stderr, "You need to provide a sudoku data file!\nUsage: ./sudoku <filename>\n");
+        fprintf(stderr, "You need to provide a sudoku data file!\nUsage: ./sudoku <filename>\n"); // Print the error message
         return -1;
     }
-    FILE *filePointer;
-    char buffer[100];
-    filePointer = fopen(argv[1], "r");
+    FILE *filePointer; // Define the file pointer
+    char buffer[100]; // Define the buffer
+    filePointer = fopen(argv[1], "r"); // Open the file
     if (filePointer == NULL)
     {
-        fprintf(stderr, "Can't Find File!\n");
+        fprintf(stderr, "Can't Find File!\n"); // Print the error message
         return 1;
     }
-    int row = 0;
-    while (fgets(buffer, sizeof(buffer), filePointer) != NULL)
+    int row = 0; // Define the row
+    while (fgets(buffer, sizeof(buffer), filePointer) != NULL) // Read the file
     {
-        // printf("%s", buffer);
-        int length = strlen(buffer);
-        token = strtok(buffer, ",");
-        for (int col = 0; token != NULL; col++)
+        token = strtok(buffer, ","); // Split the string
+        for (int col = 0; token != NULL; col++) // Loop through the columns
         {
-            sudoku[row][col] = atoi(token);
-            token = strtok(NULL, ",");
+            sudoku[row][col] = atoi(token); // Convert the string to integer
+            token = strtok(NULL, ","); // Split the string
         }
         row++;
     }
-    fclose(filePointer);
-    
-    parameters *checksquare1 = (parameters *)malloc(sizeof(parameters));
-    checksquare1->row = 0;
-    checksquare1->column = 0;
-    parameters *checksquare2 = (parameters *)malloc(sizeof(parameters));
-    checksquare2->row = 0;
-    checksquare2->column = 3;
-    parameters *checksquare3 = (parameters *)malloc(sizeof(parameters));
-    checksquare3->row = 0;
-    checksquare3->column = 6;
-    parameters *checksquare4 = (parameters *)malloc(sizeof(parameters));
-    checksquare4->row = 3;
-    checksquare4->column = 0;
-    parameters *checksquare5 = (parameters *)malloc(sizeof(parameters));
-    checksquare5->row = 3;
-    checksquare5->column = 3;
-    parameters *checksquare6 = (parameters *)malloc(sizeof(parameters));
-    checksquare6->row = 3;
-    checksquare6->column = 6;
-    parameters *checksquare7 = (parameters *)malloc(sizeof(parameters));
-    checksquare7->row = 6;
-    checksquare7->column = 0;
-    parameters *checksquare8 = (parameters *)malloc(sizeof(parameters));
-    checksquare8->row = 6;
-    checksquare8->column = 3;
-    parameters *checksquare9 = (parameters *)malloc(sizeof(parameters));
-    checksquare9->row = 6;
-    checksquare9->column = 6;
-
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_create(&tid[0], &attr, checkRow, NULL);
-    pthread_create(&tid[1], &attr, checkSquare, checksquare1);
-    pthread_create(&tid[2], &attr, checkSquare, checksquare2);
-    pthread_create(&tid[3], &attr, checkSquare, checksquare3);
-    pthread_create(&tid[4], &attr, checkSquare, checksquare4);
-    pthread_create(&tid[5], &attr, checkSquare, checksquare5);
-    pthread_create(&tid[6], &attr, checkSquare, checksquare6);
-    pthread_create(&tid[7], &attr, checkSquare, checksquare7);
-    pthread_create(&tid[8], &attr, checkSquare, checksquare8);
-    pthread_create(&tid[9], &attr, checkSquare, checksquare9);
-    pthread_create(&tid[10], &attr, checkColumn, NULL);
-
-    for (int i = 0; i < 11; i++)
-    {
-        pthread_join(tid[i], NULL);
+    fclose(filePointer); // Close the file
+    pthread_attr_init(&attr); // Initialize the thread attribute
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); // Set the thread attribute
+    // Create and start the threads
+    pthread_create(&tid[0], &attr, checkRow, NULL); // Create the thread to check the row
+    pthread_create(&tid[1], &attr, checkColumn, NULL); // Create the thread to check the column
+    parameters data[9]; // Define the data
+    for (int i = 0; i < 9; ++i) {
+        data[i].row = (i / 3) * 3; // Define the row
+        data[i].column = (i % 3) * 3; // Define the column
+        pthread_create(&tid[i + 2], &attr, checkSquare, (void *)&data[i]); // Create the thread to check the square
     }
-
-    for (int i = 0; i < 9; i++)
+    bool isSudoku = true; // Define the isSudoku
+    void *status; // Define the status
+    for (int i = 0; i < 11; i++) // Loop through the threads
+    {
+        pthread_join(tid[i], &status); // Join the thread
+        if (*(bool *)status == false) // Check the status
+        {
+            isSudoku = false; // Set the isSudoku to false
+        }
+        free(status);
+    }
+    for (int i = 0; i < 9; i++) //Output the sudoku
     {
         for (int j = 0; j < 9; j++)
         {
@@ -106,7 +87,7 @@ int main(int argc, char *argv[])
         }
         printf("\n");
     }
-    if (isValid == 11)
+    if (isSudoku) // Check the sudoku
     {
         printf("This Sudoku solution is valid\n");
     }
@@ -119,6 +100,8 @@ int main(int argc, char *argv[])
 
 void *checkRow(void *param)
 {
+    bool *isvaild=(bool *)malloc(sizeof(bool));
+    *isvaild=true;
     for (int i = 0; i < 9; i++)
     {
         int check[10] = {0};
@@ -126,18 +109,19 @@ void *checkRow(void *param)
         {
             if (check[sudoku[i][j]] == 1)
             {
-                // printf("Row %d is invalid!\n", i);
-                pthread_exit(0);
+                *isvaild=false;
+                pthread_exit(isvaild);
             }
             check[sudoku[i][j]] = 1;
         }
     }
-    isValid += 1;
-    pthread_exit(0);
+    pthread_exit(isvaild);
 }
 
 void *checkColumn(void *param)
 {
+    bool *isvaild=(bool *)malloc(sizeof(bool));
+    *isvaild=true;
     for (int i = 0; i < 9; i++)
     {
         int check[10] = {0};
@@ -145,14 +129,13 @@ void *checkColumn(void *param)
         {
             if (check[sudoku[j][i]] == 1)
             {
-                // printf("Column %d is invalid!\n", i);
-                pthread_exit(0);
+                *isvaild=false;
+                pthread_exit(isvaild);
             }
             check[sudoku[j][i]] = 1;
         }
     }
-    isValid += 1;
-    pthread_exit(0);
+    pthread_exit(isvaild);
 }
 
 void *checkSquare(void *param)
@@ -160,18 +143,19 @@ void *checkSquare(void *param)
     int row = ((parameters *)param)->row;
     int column = ((parameters *)param)->column;
     int check[10] = {0};
+    bool *isvaild=(bool *)malloc(sizeof(bool));
+    *isvaild=true;
     for (int i = row; i < row + 3; i++)
     {
         for (int j = column; j < column + 3; j++)
         {
             if (check[sudoku[i][j]] == 1)
             {
-                // printf("Square %d %d is invalid!\n", row, column);
-                pthread_exit(0);
+                *isvaild=false;
+                pthread_exit(isvaild);
             }
             check[sudoku[i][j]] = 1;
         }
     }
-    isValid += 1;
-    pthread_exit(0);
+    pthread_exit(isvaild);
 }
